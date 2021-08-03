37ed68e3b0f5b788d6caede3a03632a6d090b068
ilkaay <ilkaay.yuksel@gmail.com>
2021-08-03 16:37:44 +0300

    moving meals data to the backend and fetching meals via http

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -6,7 +6,7 @@ import CartContext from "../../../store/CartContext";
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const price = `$${props.price.toFixed(2)}`;
+  const price = `$${props.price}`;
 
   const addToCartHandler = (amount) => {
     cartCtx.addItem({
